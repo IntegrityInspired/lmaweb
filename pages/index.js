a210a2a4ca4ab365f51236ce23d1348b645ac9b1
Kevin Childs <kevinicus37@gmail.com>
2021-09-24 11:31:14 -0500

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import { Client } from "../prismic-configuration";
+import SliceZone from "next-slicezone";
+import { useGetStaticProps } from "next-slicezone/hooks";
+
+import resolver from "../sm-resolver.js";
+
+const Page = (props) => {
+  return (
+    <>
+      <h1>Hello, LMA!</h1>
+      <SliceZone {...props} resolver={resolver} />
+    </>
+  );
+};
+
+// Fetch content from prismic
+export const getStaticProps = useGetStaticProps({
+  client: Client(),
+  type: "page",
+  queryType: "repeat",
+  apiParams: {
+    uid: "home",
+  },
+});
+
+export default Page;
